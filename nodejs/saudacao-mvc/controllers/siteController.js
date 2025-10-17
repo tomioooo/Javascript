4982858e0792b7abd2392f9c18b7f235dee58072
Antonio Cardeal Pereira <cardealpereiraantonio@gmail.com>
2025-10-17 16:43:15 -0300

    Versão com rota produtos

---
+++ nodejs/saudacao-mvc/controllers/siteController.js
@@ -0,0 +1,24 @@
+const saudacaoModel = require('../models/saudacaoModel');
+
+module.exports = {
+  index: (req, res) => {
+    res.sendFile('index.html', { root: './views' });
+  },
+
+  formulario: (req, res) => {
+    res.sendFile('formulario.html', { root: './views' });
+  },
+
+  saudacao: (req, res) => {
+    const mensagem = saudacaoModel.gerarMensagemPersonalizada(nome, idade);
+    res.send(`<h1>${mensagem}</h1>`);
+  },
+
+  produtos: (req, res) => {
+    res.sendFile('produtos.html', { root: './views' });
+  },
+
+  camisetas: (req, res) => {
+    res.sendFile('camisetas.html', { root: './views' });
+  }
+};
---
+++ nodejs/saudacao-mvc/models/saudacaoModel.js
@@ -0,0 +1,17 @@
+module.exports = {
+  gerarMensagemPersonalizada: (nome, idade) => {
+    let faixaEtaria;
+
+    if (idade < 12) {
+      faixaEtaria = "criança";
+    } else if (idade < 18) {
+      faixaEtaria = "adolescente";
+    } else if (idade < 60) {
+      faixaEtaria = "adulto";
+    } else {
+      faixaEtaria = "idoso";
+    }
+
+    return `Olá, ${nome}! Você é um(a) ${faixaEtaria}.`;
+  }
+};
